e15f3e82df6a25d7cb6be3e081f07f0d4ce01d14
mistakia <1823355+mistakia@users.noreply.github.com>
2023-04-26 19:21:43 -0400

    feat: set ipv6 aaaa records too

--- scripts/set-cloudflare-dns-records.mjs
+++ scripts/set-cloudflare-dns-records.mjs
@@ -1,51 +1,82 @@
 import fetch from 'node-fetch'
 import debug from 'debug'
-// import yargs from 'yargs'
-// import { hideBin } from 'yargs/helpers'
 
-// import db from '#db'
 import config from '#config'
 import { isMain, cloudflare } from '#libs-server'
 
-// const argv = yargs(hideBin(process.argv)).argv
 const log = debug('set-cloudflare-dns-records')
 debug.enable('set-cloudflare-dns-records')
 
-const get_ip_address = async () => {
-  const response = await fetch('https://api.ipify.org?format=json')
-  const { ip } = await response.json()
-  return ip
+const get_ip_addresses = async () => {
+  const response_v4 = await fetch('https://api.ipify.org?format=json')
+  const { ip: ipv4 } = await response_v4.json()
+
+  const response_v6 = await fetch('https://api6.ipify.org?format=json')
+  const { ip: ipv6 } = await response_v6.json()
+
+  return { ipv4, ipv6 }
 }
 
 const set_cloudflare_dns_records = async () => {
-  const ip_address = await get_ip_address()
-  log(`ip_address: ${ip_address}`)
+  const { ipv4, ipv6 } = await get_ip_addresses()
+  log(`ipv4_address: ${ipv4}`)
+  log(`ipv6_address: ${ipv6}`)
 
   const base_hostname = config.base_hostname
-  const records = await cloudflare.get_records({ name: base_hostname })
-  const a_records = records.result.filter((r) => r.type === 'A')
-  const a_record = a_records[0]
 
-  if (!a_record) {
-    log('creating record')
-    const record = await cloudflare.create_record({
+  // get A records
+  const a_records = await cloudflare.get_records({
+    name: base_hostname,
+    type: 'A'
+  })
+
+  // get AAAA records
+  const aaaa_records = await cloudflare.get_records({
+    name: base_hostname,
+    type: 'AAAA'
+  })
+
+  // set A record
+  if (a_records.result.length === 0) {
+    log('creating A record')
+    await cloudflare.create_record({
       type: 'A',
       name: base_hostname,
-      content: ip_address
+      content: ipv4
     })
-    log(record)
-  } else if (a_record.content !== ip_address) {
-    log('updating record')
-    const record = await cloudflare.update_record({
-      id: a_record.id,
+  } else if (a_records.result[0].content !== ipv4) {
+    log('updating A record')
+    await cloudflare.update_record({
+      id: a_records.result[0].id,
       type: 'A',
       name: base_hostname,
-      content: ip_address
+      content: ipv4
     })
-    log(record)
   } else {
     log(
-      `no change needed, a record content: ${a_record.content} matchees ip_address: ${ip_address}`
+      `no change needed, A record content: ${a_records.result[0].content} matches ipv4_address: ${ipv4}`
+    )
+  }
+
+  // set AAAA record
+  if (aaaa_records.result.length === 0) {
+    log('creating AAAA record')
+    await cloudflare.create_record({
+      type: 'AAAA',
+      name: base_hostname,
+      content: ipv6
+    })
+  } else if (aaaa_records.result[0].content !== ipv6) {
+    log('updating AAAA record')
+    await cloudflare.update_record({
+      id: aaaa_records.result[0].id,
+      type: 'AAAA',
+      name: base_hostname,
+      content: ipv6
+    })
+  } else {
+    log(
+      `no change needed, AAAA record content: ${aaaa_records.result[0].content} matches ipv6_address: ${ipv6}`
     )
   }
 }
@@ -61,13 +92,6 @@ const main = async () => {
     console.log(error)
   }
 
-  /* await db('jobs').insert({
-   *   type: constants.jobs.EXAMPLE,
-   *   succ: error ? 0 : 1,
-   *   reason: error ? error.message : null,
-   *   timestamp: Math.round(Date.now() / 1000)
-   * })
-   */
   process.exit()
 }
 
