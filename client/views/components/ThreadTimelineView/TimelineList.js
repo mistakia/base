b5967dfb0b0a3ae3e2fe4d60d2a1357af544fcab
mistakia <1823355+mistakia@users.noreply.github.com>
2026-02-22 01:19:46 -0500

    style: consolidate timeline scroll button styling between ThreadPage and ThreadSheet

--- client/views/components/ThreadTimelineView/TimelineList.js
+++ client/views/components/ThreadTimelineView/TimelineList.js
@@ -605,15 +605,15 @@ const TimelineList = ({
             }
           }}>
           <svg
-            width='24'
-            height='24'
-            viewBox='0 0 24 24'
+            width='16'
+            height='16'
+            viewBox='0 0 16 16'
             fill='none'
             xmlns='http://www.w3.org/2000/svg'>
             <path
-              d='M7 13l5 5 5-5M7 6l5 5 5-5'
+              d='M4 9l4 4 4-4M4 3l4 4 4-4'
               stroke='currentColor'
-              strokeWidth='2'
+              strokeWidth='1.5'
               strokeLinecap='round'
               strokeLinejoin='round'
             />
